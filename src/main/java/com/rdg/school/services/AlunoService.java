c0443f5956abb364ec55e95c3e05e9742745e95e
Augxusto <rodrigo.silva.110805@gmail.com>
2026-02-25 20:45:58 -0300

    service e controller

---
+++ src/main/java/com/rdg/school/services/AlunoService.java
@@ -0,0 +1,27 @@
+package com.rdg.school.services;
+
+import com.rdg.school.models.AlunoModel;
+import com.rdg.school.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel alunoModel){
+        return  alunoRepository.save(alunoModel);
+    }
+
+    public List<AlunoModel> buscarTodosAlunos(){
+        return  alunoRepository.findAll();
+    }
+
+    public void deletarAluno(Long id){
+        alunoRepository.deleteById(id);
+    }
+}
